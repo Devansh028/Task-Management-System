2a9109c4ed4f0b23f7bcab8924c95808a5144758
Devansh028 <devanshkumar370@gmail.com>
2026-01-16 19:07:55 +0530

    fix(frontend): add default route redirect to login

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,20 @@
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import Login from "./pages/Login";
-
+import Register from "./pages/Register";
 
 function App() {
-    return <Login/>
+  return (
+    <BrowserRouter>
+      <Routes>
+        {/* Default route */}
+        <Route path="/" element={<Navigate to="/login" replace />} />
+
+        {/* Auth routes */}
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
